58e041657c1f3c27c5aa375092cba3a27fb0b13b
Thomas Lixing Yang <tboneyang@gmail.com>
2015-10-05 22:44:04 -0400

    first commit

---
+++ app/views/photos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @photo, :id, :type, :title, :created_at, :updated_at
---
+++ db/migrate/20151006022133_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.string :type
+      t.string :title
+
+      t.timestamps null: false
+    end
+  end
+end
