8e101f4bb39071bc2f4f2517b28a45e9bbbf615c
Thomas Lixing Yang <tboneyang@gmail.com>
2015-10-07 19:43:31 -0400

    added unicorn, autoprefix, masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require masonry/jquery.masonry
+//= require masonry/jquery.event-drag
+//= require masonry/jquery.imagesloaded.min
+//= require masonry/jquery.infinitescroll.min
+//= require masonry/modernizr-transitions
